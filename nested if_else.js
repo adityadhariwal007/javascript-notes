10853e0cc470b61a1cb5512f26faf9c8ad2e5ab8
adityaa250707-hue <adityaa250707@gmail.com>
2026-06-09 12:54:48 +0530

    havidhfajos

--- nested if_else.js
+++ nested if_else.js
@@ -1,27 +1,26 @@
 // nested if means if statement inside another if statement
 
 let age = 18;
-let has_license= false;
-if (age>=18){ if(has_license) 
-            {console.log("you can drive"); }
-                else {console.log("go watch pogo, kid");}
-            }
-else{
-    console.log("you are too young to drive");
+let has_license = false;
+if (age >= 18) {
+    if (has_license) { console.log("you can drive"); }
+    else { console.log("go watch pogo, kid"); }
+}
+else {
+    console.log("you are too young to drive");}
 
-// ek ek step read karte chalna hai up to down, if one gets false then jump to next line.
+    // ek ek step read karte chalna hai up to down, if one gets false then jump to next line.
 
     let marks = 85;
-let sportsQuota = true;
+    let sportsQuota = true;
 
-if (marks >= 80) {
-    if (sportsQuota) {
-        console.log("Admission with sports benefits");
+    if (marks >= 80) {
+        if (sportsQuota) {
+            console.log("Admission with sports benefits");
+        } else {
+            console.log("Regular admission");
+        }
     } else {
-        console.log("Regular admission");
-    }
-} else {
-    console.log("Admission denied");
+        console.log("Admission denied");
 
-}
-}
+    }
