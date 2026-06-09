70334da85ba6b8e6c92576a4bcc562fbf5b67ff4
adityaa250707-hue <adityaa250707@gmail.com>
2026-06-09 07:16:30 +0530

    fdgds

--- nested if_else.js
+++ nested if_else.js
@@ -8,4 +8,20 @@ if (age>=18){ if(has_license)
             }
 else{
     console.log("you are too young to drive");
+
+// ek ek step read karte chalna hai up to down, if one gets false then jump to next line.
+
+    let marks = 85;
+let sportsQuota = true;
+
+if (marks >= 80) {
+    if (sportsQuota) {
+        console.log("Admission with sports benefits");
+    } else {
+        console.log("Regular admission");
+    }
+} else {
+    console.log("Admission denied");
+
+}
 }
